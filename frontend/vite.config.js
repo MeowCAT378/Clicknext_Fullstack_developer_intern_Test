81a94fd18cffd7b0b348e28978eccffef8278931
MeowCAT378 <yothin0637354579@gmail.com>
2026-01-07 08:37:55 +0700

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,12 +4,4 @@ import vue from '@vitejs/plugin-vue'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  server: {
-    host: true,
-    port: 5173,
-    watch: {
-      usePolling: true,
-      interval: 300,
-    },
-  },
 })
